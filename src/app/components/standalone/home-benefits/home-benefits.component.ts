5e6fd833ef2f58acd72be0a1e221c083e823efd7
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2025-07-28 17:24:26 -0300

    DV-37: Create Benefits section

--- src/app/components/standalone/home-benefits/home-benefits.component.ts
+++ src/app/components/standalone/home-benefits/home-benefits.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'med-home-benefits',
   standalone: true,
   imports: [],
   templateUrl: './home-benefits.component.html',
-  styleUrl: './home-benefits.component.scss'
+  styleUrl: './home-benefits.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeBenefitsComponent {
-
+  @HostBinding('class.med-home-benefits') isActive = true;
 }
