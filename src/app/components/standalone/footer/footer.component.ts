32643470a10a48913c9adcd1aa131fee8886c284
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2025-08-16 19:52:21 -0300

    DV-38: Create Footer

--- src/app/components/standalone/footer/footer.component.ts
+++ src/app/components/standalone/footer/footer.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'med-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
-
+  @HostBinding ('class.med-footer') isActive = true;
 }
