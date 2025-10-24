61874e59fb2f20c9451c50b5bea03c68045c09e0
dppereira <diogoppereira504@gmail.com>
2025-10-23 23:04:16 -0300

    test(ui): replace home components for user home component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,11 +3,12 @@ import { HomeHeaderComponent } from '../standalone/home-header/home-header.compo
 import { HomeHeroComponent } from '../standalone/home-hero/home-hero.component';
 import { HomeBenefitsComponent } from '../standalone/home-benefits/home-benefits.component';
 import { FooterComponent } from '../standalone/footer/footer.component';
+import { WelcomeContentComponent } from '../standalone/welcome-content/welcome-content.component';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [HomeHeaderComponent, HomeHeroComponent, HomeBenefitsComponent, FooterComponent],
+  imports: [HomeHeaderComponent, WelcomeContentComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   encapsulation: ViewEncapsulation.None
