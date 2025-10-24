dedbf337429e7de7fa66df14ba86821206234af9
dppereira <diogoppereira504@gmail.com>
2025-10-23 23:12:27 -0300

    feat(ui): perfil-doctor-modal component creation

---
+++ src/app/components/standalone/perfil-doctor-modal/perfil-doctor-modal.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common'; // Import NgClass for dynamic classes
+
+export interface DoctorDetails {
+  id: number;
+  doctorPhoto: string;
+  doctorName: string;
+  doctorLastName: string;
+  doctorContact: string;
+  doctorAge: number;
+  doctorEspecialty: string;
+  doctorCRM: string;
+  doctorReview: number; // 1-5 stars
+  appointmentType: string;
+  appointmentStatus: string; // e.g., 'Realizada', 'Agendada', 'Cancelada', 'Pendente'
+  appointmentLink?: string; // Optional link for online consultations
+  reason: string; // Motivo da consulta
+  history: { date: string; type: string }[]; // Historico de consultas
+  dataHora: string; // Date and time of the current consultation
+}
+
+@Component({
+  selector: 'med-perfil-doctor-modal',
+  standalone: true,
+  imports: [CommonModule, NgClass],
+  templateUrl: './perfil-doctor-modal.component.html',
+  styleUrl: './perfil-doctor-modal.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class PerfilDoctorModalComponent {
+  @Input() appointment: DoctorDetails | null = null;
+  @Output() closeModal = new EventEmitter<void>();
+
+  getStarsArray(rating: number): number[] {
+    return Array(rating).fill(0);
+  }
+}
+
