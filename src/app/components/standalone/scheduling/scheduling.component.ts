5682390c14c51df8469b967f3a75406d72f30acd
dppereira <diogoppereira504@gmail.com>
2025-10-23 23:08:56 -0300

    feat(ui): scheduling component creation

---
+++ src/app/components/standalone/scheduling/scheduling.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'med-scheduling',
+  standalone: true,
+  imports: [],
+  templateUrl: './scheduling.component.html',
+  styleUrl: './scheduling.component.scss'
+})
+export class SchedulingComponent {
+
+}
